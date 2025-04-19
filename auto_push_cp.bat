975a41deaa8077ef7ccb3be4c419fbee41f9d556
T-Hunter14 <t.saad0432@nub.edu.eg>
2025-04-19 19:27:04 +0200

    Auto add CP solutions

---
+++ auto_push_cp.bat
@@ -0,0 +1,9 @@
+cd /d "C:\Users\T-Hunter\Desktop\CP"
+git init
+git remote remove origin
+git remote add origin https://github.com/T-Hunter14/Competitive_Programming.git
+git add .
+git commit -m "Auto add CP solutions"
+git branch -M main
+git push -u origin main
+pause
